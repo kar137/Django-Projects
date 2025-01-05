ae54437470171216941f229daa1b9e399da7976e
karanDevX11 <bistakaran89@gmail.com>
2025-01-05 20:55:23 +0545

    update author and tags data

--- Karan_BlogSite/blog/views.py
+++ Karan_BlogSite/blog/views.py
@@ -21,4 +21,5 @@ def post_detail(request, slug):
     identified_post = get_object_or_404(Post, slug=slug) 
     return render(request, 'blog/post-detail.html', {
         'post': identified_post,
+        'post_tags': identified_post.tag.all(),
     })
